7e9eddb936d8627c002781125305ce08b7853dec
Adolfo <ado.comas@gmail.com>
2023-09-04 23:09:16 -0300

    se agrega contacto y marcas

---
+++ contacto.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+   <head>
+      <meta charset="utf-8">
+      <!--[if IE]>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <![endif]-->
+      <title>Contacto SoluGraphics</title>
+      <meta content="solugraphics-chile líderes en vanguardia de impresión" name="description">
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+      <link href="assets/images/favicon.ico" rel="shortcut icon"/>
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/apple-touch-icon-144x144-precomposed.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/apple-touch-icon-114x114-precomposed.png" />
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/apple-touch-icon-72x72-precomposed.png" />
+      <link rel="apple-touch-icon-precomposed" href="assets/images/apple-touch-icon-precomposed.png" />
+      <!-- JS FILES -->
+      <script type="text/javascript" src="assets/js/jquery-1.20.2.min.js"></script>
+      <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+      <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+      <!-- CSS FILES -->
+      <link href="assets/css/style.css" media="screen" rel="stylesheet" type="text/css">
+      <link href="assets/css/responsive.css" media="screen" rel="stylesheet" type="text/css">
+      <link href="assets/rs-plugin/css/settings.css" media="screen" rel="stylesheet" type="text/css">
+      <link href="assets/css/navstylechange.css" media="screen" rel="stylesheet" type="text/css">
+      <link href="assets/css/cubeportfolio.min.css" media="screen" rel="stylesheet" type="text/css">
+      <link href="assets/css/testimonialrotator.css" media="screen" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+      <div id="wrapper">
+         <header>
+            <div class="top_line">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 pull-left">
+                        <ul class="contact-top">
+                           <li><i class="icon-location"></i> <a href="https://ul.waze.com/ul?place=ChIJa26sg2PPYpYRuS0UcghE0iQ&ll=-33.42650600%2C-70.61609400&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location">Av. Del Valle sur 576 Of 302, Huechuraba</a></li>
+                           <li><i class="icon-mobile"></i> <a href="tel:+56957625176">(+56) 9 4408 2246</a></li>
+                           <li><i class="icon-mail"></i> <a href="mailto:contacto@solugraphics-chile.com">contacto@solugraphics-chile.com</a></li>
+                        </ul>
+                     </div>
+                     <div class="col-lg-5 col-md-5 pull-right hidden-phone">
+                        <ul class="social-links link-contrast">
+                           <!--<li><a href="index.php#"><i class="fa fa-facebook"></i></a></li>-->
+                           <!--<li><a href="index.php#"><i class="fa fa-twitter"></i></a></li>-->
+                           <!--<li><a href="index.php#"><i class="fa fa-instagram"></i></a></li>-->
+                           <li><a href="https://www.linkedin.com/in/solugraphics-chile-5a1b86231/" target="_blank"><i class=" link-contrast fa fa-linkedin"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+      </div>
+      </header>
+      <div class="page_head">
+         <div class="nav-container" style="height: auto;">
+            <nav>
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-3 pull-left">
+                        <div class="logo"><a href="index.php"><img src="assets/images/logoweb.png" alt=""></a></div>
+                     </div>
+                     <div class="col-lg-9 pull-right">
+                        <div class="menu">
+                           <div id="dl-menu" class="dl-menuwrapper">
+                              <button class="dl-trigger">Menu</button>
+                              <ul class="dl-menu">
+                                 <li><a href="index.php">Inicio</a></li>
+                                 <li><a href="nosotros.php">Nosotros</a></li>
+                                 <li><a href="marcas.php">Marcas</a></li>
+                                 <li><a href="productos.php">Productos</a></li>
+                                 <li class="current"><a href="contacto.php">Contáctenos</a></li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      </div>
+      <div class="page-in">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 pull-left"><div class="page-in-name">Contacto <span>¿Necesita Soluciones? Consígalas!</span></div></div>
+                  <div class="page-in-bread"><span><i class="fa fa-map"></i></span> <a href="index.php">Inicio</a> \ Contacto</div>
+          </div>
+        </div>
+      </div>
+      <div class="container-color-fluid">
+
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.1176764489655!2d-70.617172!3d-33.394093999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c92f07e89b1d%3A0xf9f256b534b0261f!2sDel%20Valle%20576%2C%20Of%20302%2C%20Huechuraba%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1652819641529!5m2!1ses-419!2scl" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="container-color-fluid marg50">
+        <div class="row">
+          <div class="col-lg-6">
+            
+          </div> 
+        </div>
+      </div>  
+
+      <div class="container marg50">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-12">
+            <div class="promo-block">
+              <div class="promo-text link-contrast">Contáctenos</div>
+              <div class="center-line"></div>
+            </div>
+            <div class="marg50">
+              <div class="row">
+                <form id="contactForm" class="form-style" name="sentMessage"  novalidate="">
+                  <div class="col-lg-4">
+                    <div class="text_cont form-group floating-label-form-group controls">
+                      <input id="name" name="name" type="text" placeholder="Su Nombre" required="" data-validation-required-message="Ingrese su Nombre" class="form-control"><span class="help-block text-danger"></span>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="text_cont form-group floating-label-form-group controls">
+                      <input id="email" name="email" type="email" placeholder="Su Email" required="" data-validation-required-message="Ingrese su Email" class="form-control"><span class="help-block text-danger"></span>
+                    </div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="text_cont form-group floating-label-form-group controls">
+                      <input id="phone" name="phone" type="tel" placeholder="Su Teléfono" required="" data-validation-required-message="Ingrese su Teléfono" class="form-control"><span class="help-block text-danger"></span>
+                    </div>
+                  </div>
+                  <div class="col-lg-12">
+                    <div class="text_cont form-group floating-label-form-group controls">
+                      <textarea id="message" name="message" cols="40" rows="10" placeholder="Su Mensaje" required="" data-validation-required-message="Ingrese su Mensaje." aria-invalid="false" class="form-control"></textarea><span class="help-block text-danger"></span>
+                    </div>
+                  </div>
+                  <div class="col-lg-12"><div id="success"></div></div>
+                  <div class="col-lg-12"><p><button id="submit_btn" type="submit" class="btn btn-info">Enviar</button></p></div>
+                </form>
+                <div id="result"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="promo-block">
+              <div class="promo-text link-contrast">Información</div>
+              <div class="center-line"></div>
+            </div>
+            <div class="marg50">
+                <ul class="contact-footer">
+                  <li><i class="icon-location"></i> <span class="link-contrast"> Av. Del Valle sur 576 Of 302, ciudad empresarial, Huechuraba.</span></li>
+                  <li><i class="icon-mobile"></i>  <span class="link-contrast"> (+56) 9 4408 2246</span></li>
+                  <li><i class="icon-mail"></i>  <span class="link-contrast"> contacto@solugraphics-chile.com</span></li>
+                </ul> 
+                <ul class="soc-contacts">
+                  <li><a href="https://www.linkedin.com/in/solugraphics-chile-5a1b86231/"><i class="fa fa-linkedin-square link-contrast"></i></a></li>
+                </ul> 
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer" heigth="100%">
+         <div class="container">
+            <div class="row footer-colors">
+               <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="promo-text-footer">Sigamos en Contacto</div>
+                  <ul class="contact-footer">
+                     <li style="color: white"><i class="icon-location"></i> Av. Del Valle sur 576 Of 302, ciudad empresarial, Huechuraba.</li>
+                     <li style="color: white"><i class="icon-mobile"></i> (+56) 9 4408 2246</li>
+                     <li style="color: white"><i class="icon-mail"></i> contacto@solugraphics-chile.com</li>
+                     <li style="color: white"><i class="icon-clock"></i> Lunes a Viernes 08:30 - 18:00 Horas</li>
+                  </ul>
+                  <ul class="soc-footer">
+                     <!--<li><a href="index.php#"><i class="fa fa-facebook link-contrast"></i></a></li>-->
+                     <!--<li><a href="index.php#"><i class="fa fa-twitter link-contrast"></i></a></li>-->
+                     <!--<li><a href="index.php#"><i class="fa fa-instagram link-contrast"></i></a></li>-->
+                     <li><a href="index.php#"><i class="fa fa-linkedin link-contrast"></i></a></li>
+                  </ul>
+               </div>
+               <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="promo-text-footer">Marcas</div>
+                  <ul class="tags-footer">
+                  <li><a href="https://www.markandy.com" target="_blank">Mark Andy</a></li>
+                  <li><a href="http://newfoilmachines.co.uk" target="_blank">NewFoilMachinesLtda</a></li>
+                  <li><a href="https://www.creativeedgesoftware.com/ic3d/" target="_blank">Creative Edge Software</a></li>
+                  <li><a href="https://www.cla.canon.com/cla/es/home" target="_blank">Canon</a></li>
+                  <li><a href="https://www.rosas-maschinenbau.de" target="_blank">Rosas</a></li>
+                  <li><a href="https://www.agigraphics.com/es/" target="_blank">AGI</a></li>
+                  <li><a href="http://www.midamaquinaria.com/" target="_blank">MIDA</a></li>
+                  <li><a href="https://www.damitaly.com/" target="_blank">Dam Italy</a></li>
+                  <li><a href="https://www.designnbuy.com/" target="_blank">Design 'N' Buy</a></li>
+                  <li><a href="http://www.ecograph.ch/" target="_blank">Ecograph</a></li>
+                  <li><a href="https://www.kocher-beck.com/" target="_blank">Lundberg Tech</a></li>
+                  <li><a href="https://www.asahi-kasei.com/" target="_blank">AsahiKaisei</a></li>
+                  <li><a href="https://www.kocher-beck.com/" target="_blank">Kocher Beck</a></li>
+                </ul>
+               </div>
+               <div class="col-lg-4 col-md-4 col-sm-4 text-center">
+                  <div class="promo-text-footer">&nbsp;</div>
+                  <div class="flickr_widget_wrapper"><img width="50%" src="assets/images/logoFooterg.png" onmouseover="this.src='assets/images/logoFooter.png'" onmouseleave="this.src='assets/images/logoFooterg.png'"></div>
+               </div>
+            </div>
+            <div class="row footer-colors">
+               <br><br><br>
+               <div class="col-lg-6 col-md-6 col-sm-6 col-ms-12 pull-left">
+                  <div class="copyright" style="color: white">
+                     Copyright © 2022 <a href="index.php#">Solugraphics Chile</a>. &dash; Powered by 
+                     <a href="https://neandertal.cl/" target="_blank">
+                     <strong>Neandertal <span class="link-contrast">Studio</span></strong>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-6 col-md-6 col-sm-6 col-ms-12 pull-right">
+                  <div class="foot_menu">
+                     <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="marcas.php">Marcas</a></li>
+                        <li><a href="productos.php">Productos</a></li>
+                        <li><a href="contacto.php">Contáctenos</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div> 
+    </div>  
+    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/sticky.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/tweetie.js"></script>
+    <!--<script type="text/javascript" src="assets/js/form.min.js"></script>-->
+    <script type="text/javascript" src="assets/js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="assets/js/retina.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.dlmenu.js"></script>
+    <script type="text/javascript" src="assets/js/contact-form.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </body>
+</html>
---
+++ marcas.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>Nuestras Marcas - SoluGraphics</title>
+    <meta content="xenia - responsive and retina ready template" name="description">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+    <link href="assets/images/favicon.ico" rel="shortcut icon"/>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/apple-touch-icon-144x144-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/apple-touch-icon-114x114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/apple-touch-icon-72x72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" href="assets/images/apple-touch-icon-precomposed.png" />
+    <!-- JS FILES -->
+    <script type="text/javascript" src="assets/js/jquery-1.20.2.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+    <!-- CSS FILES -->
+    <link href="assets/css/style.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/responsive.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/flexslider.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="assets/css/cubeportfolio.min.css" media="screen" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="wrapper">
+      <header>
+        <div class="top_line">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 pull-left">
+                <ul class="contact-top">
+                  <li><i class="icon-location"></i> <a href="https://ul.waze.com/ul?place=ChIJa26sg2PPYpYRuS0UcghE0iQ&ll=-33.42650600%2C-70.61609400&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location">Av. Del Valle sur 576 Of 302, Huechuraba</a></li>
+                  <li><i class="icon-mobile"></i> <a href="tel:+56944082246">(+56) 9 4408 2246</a></li>
+                  <li><i class="icon-mail"></i> <a href="mailto:contacto@solugraphics-chile.com">contacto@solugraphics-chile.com</a></li>
+                </ul>
+              </div>
+                <div class="col-lg-5 col-md-5 pull-right hidden-phone">
+                   <ul class="social-links link-contrast">
+                      <!--<li><a href="index.php#"><i class="fa fa-facebook"></i></a></li>-->
+                      <!--<li><a href="index.php#"><i class="fa fa-twitter"></i></a></li>-->
+                      <!--<li><a href="index.php#"><i class="fa fa-instagram"></i></a></li>-->
+                      <li><a href="https://www.linkedin.com/in/solugraphics-chile-5a1b86231/" target="_blank"><i class=" link-contrast fa fa-linkedin"></i></a></li>
+                   </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="page_head">
+        <div class="nav-container" style="height: auto;">
+          <nav>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-3 pull-left"><div class="logo"><a href="index.php"><img src="assets/images/logoweb.png" alt=""></a></div></div>
+                <div class="col-lg-9 pull-right">
+                  <div class="menu">
+                    <div id="dl-menu" class="dl-menuwrapper">
+                    <button class="dl-trigger">Menu</button>
+                      <ul class="dl-menu">
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li class="current"><a href="marcas.php">Marcas</a></li>
+                        <li><a href="productos.php">Productos</a></li>
+                        <li><a href="contacto.php">Contáctenos</a></li>
+                      </ul>
+                    </div> 
+                  </div>        
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div class="page-in">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 pull-left"><div class="page-in-name">Alianzas <span>Estratégicas</span></div></div>
+            <div class="col-lg-6 pull-right"><div class="page-in-bread"><span><i class="fa fa-map"></i></span> <a href="index.php">Inicio</a> \ Nuestras Marcas</div></div>
+          </div>
+        </div>
+      </div>
+      <div class="container-color marg75">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="promo-block">
+              <div class="promo-text link-contrast">Nuestras Marcas</div>
+              <div class="center-line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/MarkAndy.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/NewFoilMachines.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/CreativeEdge.svg" alt=""><br><div class="text-center"></div></div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/Canon.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/Rosas.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/Ecograph.svg" alt=""><br><div class="text-center"></div></div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/AGI.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/Tesa.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/MidaLemu.svg" alt=""><br><div class="text-center"></div></div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/Dam.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/DesignnBuy.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/LundbergTech.svg" alt=""><br><div class="text-center"></div></div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/AsahiKaisei.svg" alt=""><br><div class="text-center"></div></div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12"><img width="75%" src="assets/images/svg/KocherBeck.svg" alt=""><br><div class="text-center"></div></div>
+        </div>
+      </div>
+      <div class="footer" heigth="100%">
+         <div class="container">
+            <div class="row footer-colors">
+               <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="promo-text-footer">Sigamos en Contacto</div>
+                  <ul class="contact-footer">
+                     <li style="color: white"><i class="icon-location"></i> Av. Del Valle sur 576 Of 302, ciudad empresarial, Huechuraba.</li>
+                     <li style="color: white"><i class="icon-mobile"></i> (+56) 9 4408 2246</li>
+                     <li style="color: white"><i class="icon-mail"></i> contacto@solugraphics-chile.com</li>
+                     <li style="color: white"><i class="icon-clock"></i> Lunes a Viernes 08:30 - 18:00 Horas</li>
+                  </ul>
+                  <ul class="soc-footer">
+                     <!--<li><a href="index.php#"><i class="fa fa-facebook link-contrast"></i></a></li>-->
+                     <!--<li><a href="index.php#"><i class="fa fa-twitter link-contrast"></i></a></li>-->
+                     <!--<li><a href="index.php#"><i class="fa fa-instagram link-contrast"></i></a></li>-->
+                     <li><a href="index.php#"><i class="fa fa-linkedin link-contrast"></i></a></li>
+                  </ul>
+               </div>
+               <div class="col-lg-4 col-md-4 col-sm-4">
+                  <div class="promo-text-footer">Marcas</div>
+                  <ul class="tags-footer">
+                  <li><a href="https://www.markandy.com" target="_blank">Mark Andy</a></li>
+                  <li><a href="http://newfoilmachines.co.uk" target="_blank">NewFoilMachinesLtda</a></li>
+                  <li><a href="https://www.creativeedgesoftware.com/ic3d/" target="_blank">Creative Edge Software</a></li>
+                  <li><a href="https://www.cla.canon.com/cla/es/home" target="_blank">Canon</a></li>
+                  <li><a href="https://www.rosas-maschinenbau.de" target="_blank">Rosas</a></li>
+                  <li><a href="https://www.agigraphics.com/es/" target="_blank">AGI</a></li>
+                  <li><a href="http://www.midamaquinaria.com/" target="_blank">MIDA</a></li>
+                  <li><a href="https://www.damitaly.com/" target="_blank">Dam Italy</a></li>
+                  <li><a href="https://www.designnbuy.com/" target="_blank">Design 'N' Buy</a></li>
+                  <li><a href="http://www.ecograph.ch/" target="_blank">Ecograph</a></li>
+                  <li><a href="https://www.kocher-beck.com/" target="_blank">Lundberg Tech</a></li>
+                  <li><a href="https://www.asahi-kasei.com/" target="_blank">AsahiKaisei</a></li>
+                  <li><a href="https://www.kocher-beck.com/" target="_blank">Kocher Beck</a></li>
+                </ul>
+               </div>
+               <div class="col-lg-4 col-md-4 col-sm-4 text-center">
+                  <div class="promo-text-footer">&nbsp;</div>
+                  <div class="flickr_widget_wrapper"><img width="50%" src="assets/images/logoFooterg.png" onmouseover="this.src='assets/images/logoFooter.png'" onmouseleave="this.src='assets/images/logoFooterg.png'"></div>
+               </div>
+            </div>
+            <div class="row footer-colors">
+               <br><br><br>
+               <div class="col-lg-6 col-md-6 col-sm-6 col-ms-12 pull-left">
+                  <div class="copyright" style="color: white">
+                     Copyright © 2022 <a href="index.php#">Solugraphics Chile</a>. &dash; Powered by 
+                     <a href="https://neandertal.cl/" target="_blank">
+                     <strong>Neandertal <span class="link-contrast">Studio</span></strong>
+                     </a>
+                  </div>
+               </div>
+               <div class="col-lg-6 col-md-6 col-sm-6 col-ms-12 pull-right">
+                  <div class="foot_menu">
+                     <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="nosotros.php">Nosotros</a></li>
+                        <li><a href="marcas.php">Marcas</a></li>
+                        <li><a href="productos.php">Productos</a></li>
+                        <li><a href="contacto.php">Contáctenos</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div> 
+    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
+    <script type="text/javascript" src="assets/js/sticky.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/tweetie.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="assets/js/retina.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.cubeportfolio.min.js"></script>
+    <script type="text/javascript" src="assets/js/portfolio-main.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.dlmenu.js"></script>
+    <script type="text/javascript" src="assets/js/jcarousel.responsive.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.jcarousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </body>
+</html>
